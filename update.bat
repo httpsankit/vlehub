efd7ea29f976698a86ccb9307f97e5af11fe179b
Ankit Kumar <38529870+httpsankit@users.noreply.github.com>
2024-12-22 16:00:32 +0530

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,21 +1,26 @@
 @echo off
-:: Set the URL of the file to download
-set "url=https://github.com/httpsankit/vlehub/raw/refs/heads/main/VleHub.exe"
-:: Extract the file name from the URL
+:: URLs of the files to download
+set "url1=https://github.com/httpsankit/vlehub/raw/refs/heads/main/VleHub.exe"
+set "url2=https://github.com/httpsankit/vlehub/raw/refs/heads/main/Newtonsoft.Json.dll"
+set "url3=https://github.com/httpsankit/vlehub/raw/refs/heads/main/Newtonsoft.Json.xml"
+
+:: Function to download a file
+:DownloadFile
+set "url=%~1"
 for %%I in ("%url%") do set "filename=%%~nxI"
-:: Set the destination path to the current folder with the same file name
 set "output=%~dp0%filename%"
-
-echo Downloading file...
-echo.
-:: Call PowerShell to download the file and show the progress without showing the URL
+echo Downloading %filename%...
 powershell -Command "Start-BitsTransfer -Source '%url%' -Destination '%output%'"
+echo Download of %filename% complete!
+goto :EOF
 
-echo.
-echo Download complete!
+:: Download files
+call :DownloadFile "%url1%"
+call :DownloadFile "%url2%"
+call :DownloadFile "%url3%"
 
-:: Run the downloaded file
-start "" "%output%"
+:: Run the downloaded exe file
+start "" "%~dp0VleHub.exe"
 
-:: Exit the script and close the command prompt window
+:: Exit the script
 exit
