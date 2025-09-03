29e51e616f5570973215e3ebdaa48dc793c4bbd1
Ankit Kumar <38529870+httpsankit@users.noreply.github.com>
2025-09-03 21:50:28 +0530

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,32 +1,33 @@
 @echo off
 :: -----------------------------------------
-:: URLs to download
+:: File URLs
 set "url1=https://github.com/httpsankit/Sahaj/raw/refs/heads/main/VleHub.exe"
-set "url2=https://raw.githubusercontent.com/httpsankit/Sahaj/refs/heads/main/sms.mp3"
+set "url2=https://github.com/httpsankit/Sahaj/raw/refs/heads/main/sms.mp3"
+set "url3=https://github.com/httpsankit/Sahaj/raw/refs/heads/main/chromedriver.exe"
+set "url4=https://github.com/httpsankit/Sahaj/raw/refs/heads/main/WebDriver.dll"
+set "url5=https://github.com/httpsankit/Sahaj/raw/refs/heads/main/WebDriver.Support.dll"
 :: -----------------------------------------
 
-:: Extract file names from URLs
-for %%I in ("%url1%") do set "filename1=%%~nxI"
-for %%I in ("%url2%") do set "filename2=%%~nxI"
-
-:: Set destination paths (current folder)
-set "output1=%~dp0%filename1%"
-set "output2=%~dp0%filename2%"
-
-echo Downloading files...
+:: Download function using PowerShell Invoke-WebRequest
+:download
+echo Downloading %~nx1...
+powershell -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri '%~1' -OutFile '%~2'"
+if exist "%~2" (
+    echo Downloaded: %~nx2%
+) else (
+    echo Failed to download: %~1
+)
 echo.
+exit /b
 
-:: Download first file
-powershell -NoLogo -NoProfile -Command "Start-BitsTransfer -Source '%url1%' -Destination '%output1%'"
+:: Download all files
+call :download %url1% "%~dp0VleHub.exe"
+call :download %url2% "%~dp0sms.mp3"
+call :download %url3% "%~dp0chromedriver.exe"
+call :download %url4% "%~dp0WebDriver.dll"
+call :download %url5% "%~dp0WebDriver.Support.dll"
 
-:: Download second file
-powershell -NoLogo -NoProfile -Command "Start-BitsTransfer -Source '%url2%' -Destination '%output2%'"
+:: Run main executable
+start "" "%~dp0VleHub.exe"
 
-echo.
-echo Download complete!
-
-:: Run the exe
-start "" "%output1%"
-
-:: Exit immediately (close batch window)
 exit /b
